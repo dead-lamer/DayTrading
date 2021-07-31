f8c7991e8339805edfc46916d86a154546b78a28
code-ronin6 <55556930+code-ronin6@users.noreply.github.com>
2021-07-31 17:58:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from pandas_datareader import data as pdr
+import yfinance as yf
+from datetime import datetime
+import matplotlib.pyplot as plt
+from mpl_finance import candlestick_ohlc
+import pandas as pd
+import matplotlib.dates as mpl_dates
+
+d = datetime.today().day
+m = datetime.today().month
+y = datetime.today().year
+
+start = datetime(y-1, m, d)
+end = datetime(y, m, d)
+
+def build_candle():
+    yf.pdr_override()
+    print("Choose a target: ")
+    tik = input()
+    ohlc = pdr.get_data_yahoo(tik, start=datetime(y-3, m, d), end=datetime(y, m, d), interval="1mo")
+    dates = ohlc.index
+    ohlc['Dates'] = dates
+    plt.style.use('ggplot')
+    # Extracting Data for plotting
+    ohlc = ohlc.loc[:, ['Dates', 'Open', 'High', 'Low', 'Close']]
+    ohlc['Dates'] = pd.to_datetime(ohlc['Dates'])
+    ohlc['Dates'] = ohlc['Dates'].apply(mpl_dates.date2num)
+    ohlc = ohlc.astype(float)
+    # Creating Subplots
+    fig, ax = plt.subplots()
+    candlestick_ohlc(ax, ohlc.values, width=10, colorup='green', colordown='red', alpha=0.8)
+    # Setting labels & titles
+    ax.set_xlabel('Dates')
+    ax.set_ylabel('Price')
+    fig.suptitle(f'Daily Candlestick Chart of ${tik}')
+    # Formatting Date
+    date_format = mpl_dates.DateFormatter('%d-%m-%Y')
+    ax.xaxis.set_major_formatter(date_format)
+    fig.autofmt_xdate()
+    fig.tight_layout()
+    ohlc['SMA5'] = ohlc['Close'].rolling(1).mean()
+    ax.plot(ohlc['Dates'], ohlc['SMA5'], color='green', label='SMA5')
+    plt.legend()
+    plt.show()
+
+while True:
+    build_candle()
+
+
